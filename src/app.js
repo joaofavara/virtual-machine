9543ddedda05ddf552c99f86320ace19579b87da
unknown <joaopedro_favara@hotmail.com>
2020-11-14 17:39:08 -0300

    fix node server

--- src/app.js
+++ src/app.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.use('/', serverStatic(path.join(__dirname, '/dist')));
 
-app.listen(3000, () => {
-  console.log('App is running at 3000');
-});
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`server started ${port}`);
